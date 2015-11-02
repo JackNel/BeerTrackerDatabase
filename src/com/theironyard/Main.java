16bd305b78d0adb14b0eb683c192c5ec8903686c
JackNel <jcneligan@gmail.com>
2015-11-02 16:16:54 -0500

    Add create beer and delete beer

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,16 +5,51 @@ import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static void insertBeer (Connection conn, int id, String name, String type) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO beers VALUES (?, ?, ?)");
+        stmt.setInt(1, id);
+        stmt.setString(2, name);
+        stmt.setString(3, type);
+        stmt.execute();
+    }
+
+    static void deleteBeer (Connection conn, int id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM beers WHERE id = ?");
+        stmt.setInt(1, id);
+        stmt.execute();
+    }
+
+    static ArrayList<Beer> selectBeers (Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        ResultSet results = stmt.executeQuery("SELECT * FROM beers");
         ArrayList<Beer> beers = new ArrayList();
+        while (results.next()) {
+            int id = results.getInt("id");
+            String name = results.getString("name");
+            String type = results.getString("type");
+            Beer beer = new Beer(id, name, type);
+            beers.add(beer);
+        }
+        return(beers);
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS beers (id INT, name VARCHAR, type VARCHAR)");
+
+
+
         Spark.get(
                 "/",
                 ((request, response) -> {
+                    ArrayList<Beer> beers = selectBeers(conn);
                     Session session = request.session();
                     String username = session.attribute("username");
                     if (username == null) {
@@ -40,11 +75,12 @@ public class Main {
         Spark.post(
                 "/create-beer",
                 ((request, response) -> {
+                    ArrayList<Beer> beers = selectBeers(conn);
                     Beer beer = new Beer();
                     beer.id = beers.size() + 1;
                     beer.name = request.queryParams("beername");
                     beer.type = request.queryParams("beertype");
-                    beers.add(beer);
+                    insertBeer(conn, beer.id, beer.name, beer.type);
                     response.redirect("/");
                     return "";
                 })
@@ -53,15 +89,7 @@ public class Main {
                 "/delete-beer",
                 ((request, response) -> {
                     String id = request.queryParams("beerid");
-                    try {
-                        int idNum = Integer.valueOf(id);
-                        beers.remove(idNum-1);
-                        for (int i = 0; i < beers.size(); i++) {
-                            beers.get(i).id = i + 1;
-                        }
-                    } catch (Exception e) {
-
-                    }
+                    deleteBeer(conn, Integer.valueOf(id));
                     response.redirect("/");
                     return "";
                 })
